2f8654654f6179f28bd5ac0c0668c2a0ea416555
vishu <vishwajeetsinghchauhan63@gmail.com>
2024-01-08 22:17:15 +0530

    ui changes in page.tsx

--- app/page.tsx
+++ app/page.tsx
@@ -2,6 +2,9 @@ import { Button } from "@/components/ui/button"
 
 export default function Home() {
   return (
-    <Button>Click me</Button>
-  )
+    <div className="bg-black h-screen">
+      <h1 className="text-red-500 font-bold">Hello Twitch clone (a video streaming platform)</h1>
+      <Button>Click me</Button>
+    </div>
+  );
 }
